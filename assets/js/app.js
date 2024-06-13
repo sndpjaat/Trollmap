ae96392fa9fb34636b61b495fbc869c67ba76dc5
Apple <sndpjaat008@gmail.com>
2024-06-13 10:49:30 +0530

    adjustcrd

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,28 @@ menu.addEventListener("click", () => {
     contain.style.position = "unset";
   }
 });
+// scroll to top
+
+let scroll_btn = document.querySelector(".scroll_top");
+let scroll_pt = 200;
+
+scroll_btn.classList.add("d-none");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scroll_pt) {
+        scroll_btn.classList.remove("d-none");
+    } else {
+        scroll_btn.classList.add("d-none");
+    }
+    scroll_btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
+
+AOS.init(
+    {
+        once: true,
+    }
+);
